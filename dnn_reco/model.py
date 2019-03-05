387705a41c613281f6f66c44096eecc3f09361d2
mhuen <mhuen@users.noreply.github.com>
2019-03-05 20:58:58 +0100

    pyyaml -> ruamel.yaml

--- dnn_reco/model.py
+++ dnn_reco/model.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function
 import os
 import tensorflow as tf
 import numpy as np
-import yaml
+import ruamel.yaml as yaml
 import click
 import timeit
 import glob
