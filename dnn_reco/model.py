24bb2dd369e7246b50bafe0a19b808c16bcaecf7
mhuen <mhuen@users.noreply.github.com>
2019-03-09 12:35:55 +0100

    Debug: find source of NaNs

--- dnn_reco/model.py
+++ dnn_reco/model.py
@@ -575,7 +575,10 @@ class NNModel(object):
                                        mse_values_trafo))
                     for i, name in enumerate(self.data_handler.label_names):
                         print(name, mse_values_trafo[i])
-                    print(np.isfinite(feed_dict[self.shared_objects['y_true']]).all())
+                print('Input is finite:')
+                print(np.isfinite(feed_dict[self.shared_objects['x_ic78']]).all())
+                print(np.isfinite(feed_dict[self.shared_objects['x_deepcore']]).all())
+                print(np.isfinite(feed_dict[self.shared_objects['y_true']]).all())
 
                 # every n steps: update label_weights
                 if i % self.config['validation_frequency'] == 0:
