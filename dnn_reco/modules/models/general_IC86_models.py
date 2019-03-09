cbebc43fde119b77c11b935a646887799bbc768e
mhuen <mhuen@users.noreply.github.com>
2019-03-09 14:53:30 +0100

    Debug: find source of NaNs

--- dnn_reco/modules/models/general_IC86_models.py
+++ dnn_reco/modules/models/general_IC86_models.py
@@ -209,7 +209,7 @@ def general_model_IC86_opt4(is_training, config, data_handler,
                 index_pid = data_handler.get_label_index(pid_key)
                 y_pred_list[index_pid] = tf.sigmoid(y_pred_list[index_pid])
 
-        for i, non_zero in shared_objects['non_zero_mask']:
+        for i, non_zero in enumerate(shared_objects['non_zero_mask']):
             if not non_zero:
                 y_pred_list[i] = tf.zeros_like(y_pred_list[i])
 
