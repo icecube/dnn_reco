f89bf446e9659fa0b80b4b6e0056da69d8406881
mhuen <mhuen@users.noreply.github.com>
2019-03-09 14:16:37 +0100

    Debug: find source of NaNs

--- dnn_reco/modules/models/general_IC86_models.py
+++ dnn_reco/modules/models/general_IC86_models.py
@@ -175,7 +175,7 @@ def general_model_IC86_opt4(is_training, config, data_handler,
         y_pred = tf.Print(y_pred,
                         [
                          tf.reduce_mean(y_pred, axis=0),
-                        ])
+                        ], summarize=100)
         y_pred_list = tf.unstack(y_pred, axis=1)
 
         norm = tf.sqrt(y_pred_list[index_dir_x]**2 +
