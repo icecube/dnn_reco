clip value before exp function to avoid float overflow - syntax fix

dnn_reco/data_trafo.py
@@ -565,7 +565,7 @@ class DataTransformer:
                     if log_bin:
                         if not self._tf_float_dtype == tf.float64:
                             data_list[bin_i] = \
-                                tf.clip_by_value(data_list[bin_i], -80. 80.)
+                                tf.clip_by_value(data_list[bin_i], -80., 80.)
                         data_list[bin_i] = exp_func(data_list[bin_i]) - 1.0
                 data = tf.stack(data_list, axis=-1)
             else: