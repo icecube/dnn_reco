3f5b94bdac5100c35cb8f4aa1808b797749db8f6
mhuen <mhuen@users.noreply.github.com>
2019-05-09 01:57:23 +0200

    use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+# get version number
 exec(compile(open('dnn_reco/__init__.py', "rb").read(),
              'dnn_reco/__init__.py',
              'exec'))
 
-setup(name='dnn_reco',
-      version=__version__,
-      description='DNN reconstruction for IceCube',
-      author='Mirco Huennefeld',
-      author_email='mirco.huennefeld@tu-dortmund.de',
-      url='https://github.com/mhuen/dnn_reco',
-      packages=['dnn_reco'],
-      install_requires=['numpy', 'pandas', 'click', 'ruamel.yaml', 'tables',
-                        'gitpython', 'tfscripts',
-                        ],
-      dependency_links=[
-       'git+https://github.com/mhuen/TFScripts.git@master#egg=tfscripts-0.0.1',
-      ],
-      )
+setup(
+    name='dnn_reco',
+    version=__version__,
+    description='DNN reconstruction for IceCube',
+    long_description='',
+    author='Mirco Huennefeld',
+    author_email='mirco.huennefeld@tu-dortmund.de',
+    url='https://icecube.wisc.edu/~mhuennefeld/docs/dnn_reco/html/index.html',
+    packages=['dnn_reco'],
+    install_requires=['numpy', 'pandas', 'click', 'ruamel.yaml', 'tables',
+                      'gitpython', 'tfscripts',
+                      ],
+    dependency_links=[
+      'git+https://github.com/mhuen/TFScripts.git@master#egg=tfscripts-0.0.1',
+    ],
+    )
