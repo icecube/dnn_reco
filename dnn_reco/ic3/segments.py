03490a55b8757227c3eaa8dcfaffc53c706ad968
mhuen <mhuen@users.noreply.github.com>
2019-11-28 13:46:38 +0100

    Option to define DOM exlcusions within ApplyDNNRecos. ToDo: provide method to export settings with model and check against these

--- dnn_reco/ic3/segments.py
+++ dnn_reco/ic3/segments.py
@@ -7,7 +7,7 @@ from icecube import icetray
 
 from ic3_data.container import DNNDataContainer
 from ic3_data.data import DNNContainerHandler
-from dnn_reco.ic3.modules import DeepLearningReco
+from dnn_reco.ic3.modules import DeepLearningReco, get_valid_pulse_map
 
 
 @icetray.traysegment
@@ -15,12 +15,15 @@ def ApplyDNNRecos(
         tray, name,
         pulse_key,
         model_names,
+        dom_exclusions=None,
+        partial_exclusion=True,
         output_keys=None,
         models_dir='/data/user/mhuennefeld/DNN_reco/models/exported_models',
         cascade_key='MCCascade',
         measure_time=True,
         batch_size=1,
         num_cpus=1,
+        verbose=True,
         ):
     """Apply DNN reco
 
@@ -36,6 +39,15 @@ def ApplyDNNRecos(
         A list of strings or a single string that define the models to apply.
         If a list of model names is given, the reco will be applied with each
         model.
+    dom_exclusions : list of str, optional
+        List of frame keys that define DOMs or TimeWindows that should be
+        excluded. Typical values for this are:
+        ['BrightDOMs','SaturationWindows','BadDomsList','CalibrationErrata']
+    partial_exclusion : bool, optional
+        If True, partially exclude DOMS, e.g. only omit pulses from excluded
+        TimeWindows defined in 'dom_exclusions'.
+        If False, all pulses from a DOM will be excluded if the omkey exists
+        in the dom_exclusions.
     output_keys : None, optional
         A list of output keys for the reco results.
         If None, the output will be saved as dnn_reco_{ModelName}.
@@ -53,6 +65,8 @@ def ApplyDNNRecos(
         runtime, but will also increase the memory footprint.
     num_cpus : int, optional
         Number of CPU cores to use if CPUs are used instead of a GPU.
+    verbose : bool, optional
+        If True, output pulse masking information.
     """
     if isinstance(model_names, str):
         model_names = [model_names]
@@ -69,16 +83,32 @@ def ApplyDNNRecos(
     # set up container
     container.set_up()
 
+    # mask out DOMs and TimeWindows if DOM exlcusions are provided
+    if dom_exclusions is not None:
+        tray.AddModule(get_valid_pulse_map, 'get_valid_pulse_map_' + name,
+                       pulse_key=pulse_key,
+                       dom_exclusions=dom_exclusions,
+                       partial_exclusion=partial_exclusion,
+                       verbose=verbose,
+                       )
+        container_pulse_key = pulse_key + '_masked'
+    else:
+        container_pulse_key = pulse_key
+
     tray.AddModule(DNNContainerHandler, 'DNNContainerHandler_' + name,
                    DNNDataContainer=container,
-                   PulseKey=pulse_key,
+                   PulseKey=container_pulse_key,
                    CascadeKey=cascade_key)
 
     for model_name, output_key in zip(model_names, output_keys):
-        tray.AddModule(DeepLearningReco, 'DeepLearningReco_' + model_name,
+        tray.AddModule(DeepLearningReco, 'DeepLearningReco_'+model_name+name,
                        ModelPath=os.path.join(models_dir, model_name),
                        DNNDataContainer=container,
                        OutputBaseName=output_key,
                        MeasureTime=measure_time,
                        ParallelismThreads=num_cpus,
                        )
+
+    # clean up and remove masked pulses
+    if dom_exclusions is not None:
+        tray.AddModule('Delete', 'Delete'+name, Keys=[pulse_key + '_masked'])
