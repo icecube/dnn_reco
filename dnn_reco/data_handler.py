d6029c4f955eeb9d565d14d14948683e65c86ffb
mhuen <mhuen@users.noreply.github.com>
2020-01-15 11:25:52 +0100

    restructure biased_selection: fix parameters

--- dnn_reco/data_handler.py
+++ dnn_reco/data_handler.py
@@ -898,7 +898,7 @@ class DataHandler(object):
         for process in self._mp_processes:
             process.join(timeout=1.0)
 
-    def _create_model(self, cfg_sel):
+    def _create_model(self, cfg, cfg_sel):
         """Helper function for biased selection. Creates a new DNN model in an
         independent graph
 
@@ -994,7 +994,8 @@ class DataHandler(object):
             raise ValueError('Biased fraction {!r} not in (0, 1]'.format(
                 cfg_sel['biased_fraction']))
 
-        model, data_transformer, data_handler = self._create_model(cfg_sel)
+        model, data_transformer, data_handler = \
+            self._create_model(cfg, cfg_sel)
 
         # create nn biased selection masking function
         def nn_biased_selection_func(icecube_data):
